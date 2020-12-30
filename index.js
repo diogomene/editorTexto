b1d4918fd8cffa20e6357bdedf0698e25c0ed321
diogomene <diogodelimamenezes15@gmail.com>
2020-12-30 03:38:13 -0400

    Ultima tentativa de rodar em dominios diferentes

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 
 const express = require('express');
 const app= express();
-const server = require('https').createServer(app);
-
-  app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-  });
+const server = require('http').createServer(app);
 
 porta = process.env.PORT || 3000;
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {cors: {
+    origin: '*',
+  }});
 
 app.use(express.static(__dirname+"/public"));
 app.set('views', __dirname+"/public");
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html')
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 //socket
+    app.use(allowCrossDomain);
+
+
 app.set('origins','*:*')
 io.on('connection', socket=>{
     console.log("Socket conectado: +"+socket.id);
