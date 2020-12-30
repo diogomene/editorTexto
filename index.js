95e225159fd41a85807a6094e70d2d8ddcd3c4e7
diogomene <diogodelimamenezes15@gmail.com>
2020-12-30 00:25:48 -0400

    Primeiro comit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app= express();
+const server = require('http').createServer(app);
+porta = process.env.PORT || 3000;
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname+"/public"));
+app.set('views', __dirname+"/public");
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html')
+
+
+//socket
+io.on('connection', socket=>{
+    console.log("Socket conectado: +"+socket.id);
+    socket.on('textoMod', data=>{
+        console.log(data);
+        socket.broadcast.emit('newText',data)
+    })
+})
+
+
+app.use('/', (req,res)=>{
+    res.render('index.html');
+})
+server.listen(porta, ()=>{
+    console.log("Server running at: "+ porta)
+})
