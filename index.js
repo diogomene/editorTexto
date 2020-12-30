e332e0b25880ef5bf46144fccac78c65aa2f82fc
diogomene <diogodelimamenezes15@gmail.com>
2020-12-30 03:41:19 -0400

    Ultimas modificações

--- index.js
+++ index.js
@@ -13,24 +13,8 @@ app.set('views', __dirname+"/public");
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html')
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
 //socket
-    app.use(allowCrossDomain);
-
-
-app.set('origins','*:*')
+    // app.use(allowCrossDomain);
 io.on('connection', socket=>{
     console.log("Socket conectado: +"+socket.id);
     socket.on('textoMod', data=>{
